da2742ee8ff5efc360eb23734405d77606a9a37d
Zach Jansen <zjansen@drury.edu>
2017-05-10 02:29:10 -0500

    Email Entry Code, Not Finished

--- CodeBase/wildlife/app/src/main/java/edu/drury/mcs/wildlife/Fragment/EmailDialog.java
+++ CodeBase/wildlife/app/src/main/java/edu/drury/mcs/wildlife/Fragment/EmailDialog.java
@@ -6,7 +6,11 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
@@ -17,7 +21,18 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.supercsv.io.CsvMapWriter;
+import org.supercsv.io.ICsvMapWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import java.io.Console;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
 import edu.drury.mcs.wildlife.Activity.CreateCollection;
+import edu.drury.mcs.wildlife.DB.wildlifeDBHandler;
 import edu.drury.mcs.wildlife.JavaClass.CollectionObj;
 import edu.drury.mcs.wildlife.JavaClass.Message;
 import edu.drury.mcs.wildlife.R;
@@ -33,9 +48,22 @@ public class EmailDialog extends DialogFragment {
     private AlertDialog.Builder dialogBuilder;
     private LayoutInflater inflater;
     private View dialog_view;
-    private Button cancel, create;
+    private Button cancel, send;
     private EditText editText;
     private Context context;
+
+    public HashMap<String, Object> getANSWER() {
+        return ANSWER;
+    }
+
+    public void setANSWER(HashMap<String, Object> ANSWER) {
+        this.ANSWER = ANSWER;
+    }
+
+    private HashMap<String, Object> ANSWER;
+    boolean isAlreadyExist = false;
+    private File file = null;
+    private static final String CSV_HEADER[] = {"EntryName"};
 
     public EmailDialog(Context context) {
         this.context = context;
@@ -44,6 +72,7 @@ public class EmailDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
         dialogBuilder = new AlertDialog.Builder(getActivity());
 
         //get the layout inflater
@@ -55,7 +84,7 @@ public class EmailDialog extends DialogFragment {
 
         // get hold of edittext, cancel and create button and set behavior
         editText = (EditText) dialog_view.findViewById(R.id.name_collection);
-        create = (Button) dialog_view.findViewById(R.id.create);
+        send = (Button) dialog_view.findViewById(R.id.send_entry);
         cancel = (Button) dialog_view.findViewById(R.id.cancel);
 
         cancel.setOnClickListener(new View.OnClickListener() {
@@ -67,10 +96,23 @@ public class EmailDialog extends DialogFragment {
             }
         });
 
-        create.setOnClickListener(new View.OnClickListener() {
+        send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CollectionObj currentCollection = new CollectionObj();
+                //Generate CSV and save on phone
+                exportDBToCSV();
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + Environment.getExternalStorageDirectory()
+                                + "/" + "Android/data/" + context.getPackageName().toString() + "/CSV.csv"));
+                //sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+                sendIntent.setType("text/csv");
+                startActivity(Intent.createChooser(sendIntent, "Send mail..."));
+
+                //Send intent with CSV file to send to another app (i.e. Outlook Email)
+
+                /*CollectionObj currentCollection = new CollectionObj();
                 String collection_name = editText.getText().toString().trim();
                 Log.i(TAG,collection_name);
                 if(!collection_name.equals("")) {
@@ -85,10 +127,82 @@ public class EmailDialog extends DialogFragment {
                 } else {
                     Message.showMessage(context,"Collection Name is required");
                 }
-
+*/
             }
         });
 
         return dialogBuilder.create();
     }
+
+    private void exportDBToCSV() {
+
+        //setANSWER("Entry1", CSV_HEADER[0]);
+        //ANSWER.put(CSV_HEADER[0], "Entry1");
+        fileHandler();
+        writeDataOnCSV();
+
+        /*File dbFile = context.getDatabasePath("wildlife.db");
+        wildlifeDBHandler dbhelper = new wildlifeDBHandler(context);
+        File exportDir = new File(Environment.getExternalStorageDirectory(), "");
+        if (!exportDir.exists()) {
+            exportDir.mkdirs();
+        }
+
+        File file = new File(exportDir, "collectionEntry.csv");
+        try {
+            file.createNewFile();
+            CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
+            SQLiteDatabase db = dbhelper.getReadableDatabase();
+            Cursor curCSV = db.rawQuery("SELECT * FROM contacts", null);
+            csvWrite.writeNext(curCSV.getColumnNames());
+            while (curCSV.moveToNext()) {
+                //Which column you want to exprort
+                String arrStr[] = {curCSV.getString(0), curCSV.getString(1), curCSV.getString(2)};
+                csvWrite.writeNext(arrStr);
+            }
+            csvWrite.close();
+            curCSV.close();
+        } catch (Exception sqlEx) {
+            Log.e("MainActivity", sqlEx.getMessage(), sqlEx);
+        }*/
+    }
+
+    private void fileHandler() {
+        File mainDir = new File(Environment.getExternalStorageDirectory() + "/" + "Android/data/" +
+                context.getPackageName().toString());
+
+        if (!mainDir.exists()) {
+            mainDir.mkdir();
+        }
+
+        file = new File(mainDir + "/" + "CSV.csv");
+
+        if (!file.exists()) {
+            isAlreadyExist = true;
+        }
+    }
+
+    private void writeDataOnCSV() {
+        ICsvMapWriter mapWriter = null;
+        try {
+            mapWriter = new CsvMapWriter(new FileWriter(file, true), CsvPreference.STANDARD_PREFERENCE);
+
+            if (isAlreadyExist) {
+                mapWriter.writeHeader(CSV_HEADER);
+            }
+
+            mapWriter.write(getANSWER(), CSV_HEADER);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (mapWriter != null) {
+                try {
+                    mapWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
